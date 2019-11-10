26480b076c57115963446af450cbffa3790fae00
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2019-11-09 17:11:06 -0800

    Update server.py

--- src/server.py
+++ src/server.py
@@ -1,16 +1,21 @@
-"""
-Basic Internet Relay Chat (IRC) server program.
-Some source code modified from: http://net-informations.com/python/net/thread.htm
-"""
-
 import socket, threading
 
+in_room = 0
 rooms = []
+
+class room():
+    def __init__(self, room_number):
+        self.room_number = room_number
+        self.rlist = [] #List to keep track of who is in the room
+	
+    def update_rlist(self, clientAddr, clientSocket):
+        self.rlist.append({clientAddr, clientSocket})
 
 class ClientThread(threading.Thread):
     def __init__(self, clientAddress, clientsocket):
         threading.Thread.__init__(self)
         self.csocket = clientsocket
+        self.caddr = clientAddress
         print ("New connection added: ", clientAddress)
 		
     def run(self):
@@ -26,12 +31,32 @@ class ClientThread(threading.Thread):
               print("create room detected")
             elif msg == '2':
               print("join room detected")
+              in_room = 1
+              data = self.csocket.recv(1024)
+              room_num = data.decode()
+              #rooms.append(room(room_num))
+              new_room = room(room_num)
+              new_room.update_rlist(self.caddr, self.csocket)
+              rooms.append(new_room)
+              while in_room == 1:
+                data = self.csocket.recv(1024)
+                msg = data.decode()
+                if msg == 'EXIT':
+                  in_room = 0
+                else:
+                  self.csocket.send(bytes(msg, 'UTF-8'))
             elif msg == '3':
               print("leave room detected")
             elif msg == '4':
               print("list rooms detected")
-            print ("from client", msg)
-            self.csocket.send(bytes(msg,'UTF-8'))
+              #self.csocket.send(bytes("Here are the current rooms",'UTF-8'))
+              if len(rooms) == 0:
+                self.csocket.send(bytes("No rooms available.", 'UTF-8'))
+              else:
+                for i in range(len(rooms)):
+                  self.csocket.send(bytes(i, 'UTF-8'))
+            #print ("from client", msg)
+            #self.csocket.send(bytes(msg,'UTF-8'))
         print ("Client at ", clientAddress , " disconnected...")
 		
 LOCALHOST = "127.0.0.1"
@@ -43,6 +68,7 @@ print("Server started")
 print("Waiting for client request..")
 
 while True:
+    print("Len of rooms is: ", len(rooms))
     server.listen(1)
     clientsock, clientAddress = server.accept()
     newthread = ClientThread(clientAddress, clientsock)
