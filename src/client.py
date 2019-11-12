06797f8840d4c95abb59fa510e815d0b576cce43
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2019-11-11 22:30:25 -0800

    Update client.py

--- src/client.py
+++ src/client.py
@@ -1,10 +1,17 @@
+"""
+Some source code modified from: http://net-informations.com/python/net/thread.htm
+"""
+
+
 import socket
 
 SERVER = "127.0.0.1"
 PORT = 8080
+
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((SERVER, PORT))
 #client.sendall(bytes("This is from Client",'UTF-8'))
+client.setblocking(True)
 in_room = 0
 
 print("Enter 1 to create a room, 2 to join a room, 3 to leave a room, and 4 to list the rooms. Type exit to leave.")
@@ -15,7 +22,17 @@ while True:
   if out_data == '1':
     print("Create room detected.")
     client.sendall(bytes(out_data,'UTF-8'))
-  if out_data == '2':
+    while True:
+      print("in while loop")
+      #in_data = client.recv(1024)
+      out_data = input()
+      client.sendall(bytes(out_data, 'UTF-8'))
+      in_data = client.recv(1024)
+      print("From chatroom :", in_data.decode())
+      if out_data == 'EXIT':
+        break
+        print("Leaving room ... ")
+  elif out_data == '2':
     client.sendall(bytes(out_data,'UTF-8'))
     print("Join room detected. Please enter the room number you want to join: ")
     out_data = input()
@@ -32,13 +49,15 @@ while True:
       if out_data == 'EXIT':
         in_room = 0
         print("Leaving room ... ")
-  if out_data == '3':
+  elif out_data == '3':
     print("Not currently in a room, can't leave room.")
-  if out_data == '4':
+    client.sendall(bytes(out_data,'UTF-8'))
+  elif out_data == '4':
     client.sendall(bytes(out_data, 'UTF-8'))
     in_data = client.recv(1024)
     print(in_data.decode())
   #client.sendall(bytes(out_data,'UTF-8'))
   if out_data=='exit':
     break
+	
 client.close()
