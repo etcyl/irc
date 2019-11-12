ec6e0b19853f44ccd910415a78e513ab299d84fc
Matt Fleetwood <32446381+etcyl@users.noreply.github.com>
2019-11-12 02:37:09 -0800

    Update client.py

--- src/client.py
+++ src/client.py
@@ -14,8 +14,8 @@ client.connect((SERVER, PORT))
 client.setblocking(True)
 in_room = 0
 
-class InputThread(threading.Thread):
-    def __init__(self, client):
+class InputThread(threading.Thread): # Process receiving server data separetely from input from the user
+    def __init__(self, client):      # This allows for the most current chatroom messages to display regardless of the user's input 
         threading.Thread.__init__(self)
         self.client = client
 		
@@ -25,55 +25,43 @@ class InputThread(threading.Thread):
           data = self.client.recv(1024)
           msg = data.decode()
           if msg=='EXIT':
+            print("Leaving room ... ")
             break
           print ("From chatroom: ", msg)
 
-print("Enter 1 to create a room, 2 to join a room, 3 to leave a room, and 4 to list the rooms. Type exit to leave.")
+print("Enter 1 to create a room, 2 to join a room, and 3 to list the rooms. Type exit to leave.")
 while True:
   #in_data =  client.recv(1024)
   #print("From Server :" ,in_data.decode())
   out_data = input()
-  if out_data == '1':
+  if out_data == '1': # Client wants to create a room
     print("Create room detected.")
     client.sendall(bytes(out_data,'UTF-8'))
     stream_thread = InputThread(client)
     stream_thread.start()
     while True:
-      print("in while loop")
-      #in_data = client.recv(1024)
       out_data = input()
+      if out_data == 'EXIT':
+       break
       client.sendall(bytes(out_data, 'UTF-8'))
-      #in_data = client.recv(1024)
-      #print("From chatroom :", in_data.decode())
-      #if out_data == 'EXIT':
-        #break
-        #print("Leaving room ... ")
-  elif out_data == '2':
+  elif out_data == '2': # Client wants to join an existing room
     client.sendall(bytes(out_data,'UTF-8'))
     print("Join room detected. Please enter the room number you want to join: ")
     out_data = input()
     client.sendall(bytes(out_data, 'UTF-8'))
-    in_room = 1
-    print("Joining room number ", out_data, "enter EXIT to leave the room")
-    while in_room == 1:
+    print("Joining room number ", out_data, ", enter EXIT to leave the room")
+    stream_thread = InputThread(client)
+    stream_thread.start()
+    while True:
       out_data = input()
-      client.sendall(bytes(out_data, 'UTF-8'))
-      in_data = client.recv(1024)
-      print("From chatroom :", in_data.decode())
-      #out_data = input()
-      #client.sendall(bytes(out_data, 'UTF-8'))
       if out_data == 'EXIT':
-        in_room = 0
-        print("Leaving room ... ")
-  elif out_data == '3':
-    print("Not currently in a room, can't leave room.")
-    client.sendall(bytes(out_data,'UTF-8'))
-  elif out_data == '4':
+        break
+      client.sendall(bytes(out_data, 'UTF-8'))
+  elif out_data == '3': # Client wants to list the current rooms
     client.sendall(bytes(out_data, 'UTF-8'))
     in_data = client.recv(1024)
     print(in_data.decode())
-  #client.sendall(bytes(out_data,'UTF-8'))
-  if out_data=='exit':
+  if out_data=='exit': # Client wants to exit the program
     break
 	
 client.close()
